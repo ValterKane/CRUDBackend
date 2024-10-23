6f2e79720664238b192117ebbbb88edf6446af3c
ValterKane <zhukov.petr86@yandex.ru>
2024-10-23 18:23:40 +0300

    12

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -32,13 +32,8 @@ foreach (var listOfClasses in repositoryService.SelectMany(repo => repo.Value))
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.MapControllers();
 
